f48c3ef5f6a6ff549d88b33ae717331d686048ee
washogo <aiz4338150@gmail.com>
2022-02-12 09:41:11 +0900

    Finish the lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
+import { Text } from './Text';
+import { Todo } from './Todo';
+import { TodoType } from "./types/todo"
+import { User } from './types/User';
+import { UserProfile } from './UserProfile';
+
+const user: User = {
+  name: "しょご",
+  // hobbies: ["映画", "ゲーム"]
+}
 
 function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+  
+  const onClickFetchData = () => {
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserProfile user={user}/>
+      <Text color="red" fontSize="18px" />
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/practices/typeList.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+/* TypeScriptの基本の型 */
+export {}
+// boolean
+let bool: boolean = true;
+
+// number 数値
+let number: number = 0;
+
+// string 文字
+let string: string = "文字";
+
+// array 配列
+let arr1: Array<number> = [1, 2, 3, 4, 5];
+let arr2: number[] = [1, 2, 3, 4, 5];
+
+// tuple
+let tuple: [number, string] = [0, "A"];
+
+// any
+let any1: any = false;
+
+// void
+const funcA = () => {
+  const test = "TEST";
+}
+
+// null
+let null1: null = null;
+
+// undefined
+let undefined1: undefined = undefined;
+
+// object
+let obj1: object = {};
+let obj2: { id: number, name: string } = { id: 0, name: "AA"};
